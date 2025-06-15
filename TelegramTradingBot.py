06dedf82f3ca2f1f13beb9d5a644193d23b95062
Unik228 <arnauldkoudanou220391@gmail.com>
2025-06-15 16:39:39 +0000

    Update TelegramTradingBot.py

--- TelegramTradingBot.py
+++ TelegramTradingBot.py
@@ -1,3 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
+BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
+BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET")
 import time
 import hmac
 import hashlib
